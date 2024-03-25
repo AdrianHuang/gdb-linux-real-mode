4e4996a7344f83ec1656e118793ab42eee9bbee6
Adrian Huang <adrianhuang0701@gmail.com>
2024-03-25 15:56:25 +0800

    Add 'megasas' device

--- scripts/launch-vm.sh
+++ scripts/launch-vm.sh
@@ -9,6 +9,7 @@ ${qemu_bin} -nographic -smp cores=4,sockets=2 -cpu Nehalem -m 16G \
 	-object memory-backend-ram,id=mem1,size=8G \
         -numa node,memdev=mem0,cpus=0-3,nodeid=0 \
         -numa node,memdev=mem1,cpus=4-7,nodeid=1 \
+	-device megasas,id=scsi0 \
 	-kernel $KERNEL_OBJ/arch/x86/boot/bzImage \
 	-initrd $INITRAMFS_IGZ \
 	-append "earlyprintk=serial,ttyS0 console=ttyS0 loglevel=8 nokaslr norandmaps" \
